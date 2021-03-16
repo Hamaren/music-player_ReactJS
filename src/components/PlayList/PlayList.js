9bd8d8a467c9fe19affd27c51ac679e70f7ba76a
Vlad <hamaren01@gmail.com>
2021-03-16 23:25:52 +0300

    added: styles, playback animation

--- src/components/PlayList/PlayList.js
+++ src/components/PlayList/PlayList.js
@@ -1,9 +1,11 @@
 import {useSelector, useDispatch} from 'react-redux';
 import {jump} from '../../redux/actions/controls';
+import './style.scss';
 
 function PlayList(){
   const songs = useSelector(state => state.playList.songs);
   const currentSongIndex = useSelector(state => state.control.currentSongIndex);
+  const isPlaying = useSelector(state => state.control.isPlaying);
   const dispatch = useDispatch();
 
   return(
@@ -11,7 +13,17 @@ function PlayList(){
       {songs.map((song, key)=>{
         return(
           <div className="song" key={key} onClick={()=> dispatch(jump(key))}>
-            <div className="song__title" style={{'color': key === currentSongIndex ? 'blue' : 'black'}}>{song.title}</div>
+            {key === currentSongIndex && isPlaying ? (
+              <svg className="song__eq" xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 20 20" fill="#fff">
+                <rect className="eq-bar eq-bar--1" x="1" y="4" width="3.7" height="8"/>
+                <rect className="eq-bar eq-bar--2" x="7.2" y="4" width="3.7" height="16"/>
+                <rect className="eq-bar eq-bar--3" x="13.3" y="4" width="3.7" height="11"/>
+              </svg>
+            ) : null}
+            <div className="song__title" style={{'color': key === currentSongIndex ? 'blue' : null}}>
+              <span>{key + 1}. </span>
+              {song.title}
+            </div>
           </div>
         )
       })}
--- src/redux/reducers/playListReducer.js
+++ src/redux/reducers/playListReducer.js
@@ -16,12 +16,6 @@ const initialState = {
       title: 'test 4',
       src: './music/test4.mp3'
     },
-    // './music/test5.mp3',
-    // './music/test3.mp3',
-    // // './music/test3.mp3',
-    // './music/test.mp3',
-    // // './music/test2.mp3',
-    // './music/test4.mp3',
   ]
 };
 
